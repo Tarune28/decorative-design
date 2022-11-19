a8c877605d2bf3b53d1ffa893cca67083712b88f
taruneswar <tarun.eswar15@gmail.com>
2022-11-19 11:25:04 -0500

    migration to github

---
+++ contact_process.php
@@ -0,0 +1,38 @@
+<?php
+
+if(isset($_REQUEST['email'])):
+    $to = "tarun.eswar15@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $cmessage = $_REQUEST['message'];
+endif;
+	
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $logo = 'img/logo.png';
+    $link = '#';
+
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: left;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "</tr>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt='logo' ></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "</tr>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+?>
